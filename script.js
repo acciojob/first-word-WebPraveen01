de252cfc83d588c9904d03c295da4386b1396402
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-10-25 22:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
+  s = str.trim();
+    
+    // Check if the string is empty
+    if (str === "") {
+        return "";
+    }
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If there is no space, return the entire string; otherwise, return the substring before the space
+    return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String: see and stop");
 alert(firstWord(s));
